729fe8c8c1411ad1ad82f21620a7a34e703fea8c
Vincent Berenz <vberenz@tuebingen.mpg.de>
2020-06-16 21:24:42 +0200

    finalized burst demo

--- demos/burst_demo.py
+++ demos/burst_demo.py
@@ -8,36 +8,22 @@ import o80_pam
 segment_id = "o80_pam"
 frontend = o80_pam.FrontEnd(segment_id)
 
-time.sleep(1)
+# getting to 15000 one iteration at a time
+for iteration in range(15000):
+    target = [iteration]*4
+    frontend.add_command(target,
+                         target,
+                         o80.Mode.OVERWRITE)
+    obs = frontend.burst(1)
+    new_iteration = obs.get_iteration()
+    desired = obs.get_desired_pressures()
+    print(new_iteration,target,desired)
 
+# going back to 0 over 10000 iteration,
+# played in one burst
 current_iteration = frontend.latest().get_iteration()
-target = 15000
-frontend.add_command([target]*4,
-                     [target]*4,
-                     o80.Mode.OVERWRITE)
-obs = frontend.burst(2)
-new_iteration = obs.get_iteration()
-desired = obs.get_desired_pressures()
-print("start iter:",current_iteration,"end iteration:",new_iteration,
-      "target state:",target,"final desired state:",desired)
-
-
-
-#time.sleep(2)
-
-# going to 15000 for all muscles, 1 iteration at a time
-def comment():
-    for iteration in range(15000):
-        current_iteration = frontend.latest().get_iteration()
-        target = [iteration]*4
-        frontend.add_command(target,
-                             target,
-                             o80.Iteration(current_iteration+1,False),
-                             o80.Mode.OVERWRITE)
-        obs = frontend.burst(1)
-        new_iteration = obs.get_iteration()
-        desired = obs.get_desired_pressures()
-        print(current_iteration,new_iteration,target,desired)
-
-
-
+frontend.add_command([0]*4,
+                     [0]*4,
+                     o80.Iteration(current_iteration+10000),
+                     o80.Mode.QUEUE)
+frontend.burst(10000)
