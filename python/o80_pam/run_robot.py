42d0a98ba7f643d5a4dfbb06aa3ec8aa88731605
Vincent Berenz <vberenz@tuebingen.mpg.de>
2020-10-20 21:32:24 +0200

    o80_dummy working

--- python/o80_pam/run_robot.py
+++ python/o80_pam/run_robot.py
@@ -5,26 +5,43 @@ import time
 import o80
 import o80_pam
 import pam_interface
+import signal_handler
 from lightargs import BrightArgs,Set,Range,Positive,FileExists
 
 # code used for starting o80 pam
 # in the executable o80_pam/o80_real and
 # o80_pam/o80_dummy 
 
+def _set_min_pressures(config,pam_config):
+
+    frontend = o80_pam.FrontEnd(config.segment_id)
+    for dof in range(config.nb_dofs):
+        min_ago = pam_config.min_pressure(dof,pam_interface.sign.agonist)
+        min_antago = pam_config.min_pressure(dof,pam_interface.sign.antagonist)
+        frontend.add_command(dof,min_ago,min_antago,o80.Mode.OVERWRITE)
+    frontend.pulse()
+    del frontend
 
 def run(config):
 
+    log_handler = logging.StreamHandler(sys.stdout)
+    logging.basicConfig(
+        format="[o80 PAM segment_id: {} |  %(levelname)s %(asctime)s] %(message)s".format(config.segment_id),
+        level=logging.DEBUG,
+        handlers=[log_handler]
+    )
+
+    logging.info("read configuration") 
     segment_id = config.segment_id
     frequency = config.frequency
     bursting_mode = config.bursting_mode
-    pam_config = pam_interface.JsonConfiguration(config.pam_config_file)
     robot = config.robot
+    pam_config = pam_interface.JsonConfiguration(config.pam_config_file)
 
-    # starting from a clean state
+    logging.info("cleaning shared memory") 
     o80.clear_shared_memory(segment_id)
 
-    # starting the standalone (hosts the driver to pam
-    # and the o80 backend)
+    logging.info("starting standalone") 
     if robot=="real":
         o80_pam.real_start_standalone(segment_id,
                                       frequency,
@@ -35,36 +52,31 @@ def run(config):
                                        frequency,
                                        bursting_mode,
                                        pam_config)
-        
-    # setting desired pressures to config minimal pressures
-    frontend = o80_pam.FrontEnd(segment_id)
-    for dof in range(config.nb_dofs):
-        min_ago = pam_config.min_pressure(dof,pam_interface.sign.agonist)
-        min_antago = pam_config.min_pressure(dof,pam_interface.sign.antagonist)
-        frontend.add_command(dof,min_ago,min_antago,o80.Mode.OVERWRITE)
-    frontend.pulse()
-    del frontend
 
+    logging.info("setting pressures to minimal values")         
+    _set_min_pressures(config,pam_config)
+    logging.info("init exit signal handler")         
+    signal_handler.init()
     try:
-        while True:
+        time_c = time.time()-6
+        while not signal_handler.has_received_sigint():
             time.sleep(0.01)
-    except KeyboardInterrupt:
-        pass
+            if time.time()-time_c > 5:
+                logging.info("running ...")
+                time_c = time.time()
+    except (KeyboardInterrupt,SystemExit):
+        logging.info("exiting ...")
     except Exception as e:
-        print("\n\nException: {}\n".format(e))
+        logging.error(str(e))
     finally:
-        # setting desired pressures to config minimal pressures
-        # upon exit
-        frontend = o80_pam.FrontEnd(segment_id)
-        for dof in range(config.nb_dofs):
-            min_ago = pam_config.min_pressure(dof,pam_interface.sign.agonist)
-            min_antago = pam_config.min_pressure(dof,pam_interface.sign.antagonist)
-            frontend.add_command(dof,min_ago,min_antago,
-                                 o80.Speed.per_second(2000),o80.Mode.OVERWRITE)
-        frontend.pulse_and_wait()
-        del frontend
-        o80_pam.stop_standalone(segment_id)
-
+        logging.info("setting pressures to minimal values")         
+        _set_min_pressures(config,pam_config)
+        logging.info("stopping standalone")
+        o80_pam.stop_standalone(config.segment_id)
+        logging.info("cleaning shared memory") 
+        o80.clear_shared_memory(segment_id)
+        logging.info("exit")
+        
         
 def configure(real_robot):
     if real_robot:
@@ -96,4 +108,5 @@ def configure(real_robot):
                       integrity_checks= [FileExists()])
     change_all = False
     config.dialog(change_all)
+    print()
     return config
