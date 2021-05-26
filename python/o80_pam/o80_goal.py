32791e9271627d47095f059ae0b5fccc00b91790
Vincent Berenz <vberenz@tuebingen.mpg.de>
2021-05-26 15:48:27 +0200

    add o80_introspector, updated code to handles

--- python/o80_pam/o80_goal.py
+++ python/o80_pam/o80_goal.py
@@ -3,5 +3,5 @@ from .o80_ball import o80Ball
 
 class o80Goal(o80Ball):
 
-    def __init__(self,segment_id):
-        o80Ball.__init__(self,segment_id)
+    def __init__(self,segment_id,frontend=None):
+        o80Ball.__init__(self,segment_id,frontend)
