ea28f7e5642549a14557f5e8caa4309955c50ae4
Vincent Berenz <vberenz@ocean.is.localnet>
2023-05-16 12:36:03 +0200

    added o80_real_ball

--- python/o80_pam/__init__.py
+++ python/o80_pam/__init__.py
@@ -1,6 +1,7 @@
 from o80_pam_wrp import *
 
 from .o80_ball import o80Ball
+from .o80_real_ball import o80RealBall
 from .o80_hit_point import o80HitPoint
 from .o80_goal import o80Goal
 from .o80_pressures import o80Pressures
@@ -12,6 +13,7 @@ from .mujoco_id import mujoco_id
 from .logger import Logger, read_file, FileManager
 from .run_robot import run, run_dummy_robot
 from . import robot_ball_parser
+from . import observation_convertors
 
 BallFrontEnd = MirrorFreeJointFrontEnd
 GoalFrontEnd = MirrorFreeJointFrontEnd
---
+++ python/o80_pam/o80_real_ball.py
@@ -0,0 +1,24 @@
+import tennicam_client
+
+
+class o80RealBall:
+    """
+    Wrapper over a tennicam_client.Frontend.
+
+    Arguments:
+      segment_id: o80 segment-id of the running tennicam client
+    """
+
+    def __init__(self, segment_id: str = "tennicam_client") -> None:
+        self._frontend = tennicam_client.FrontEnd(tennicam_client)
+
+    def get(self) -> tuple[int, list[float], list[float]]:
+        """
+        Read the latest known time stamp, position and velocity
+        of the ball as detected by tennicam.
+        """
+        observation = self._frontend.pulse()
+        time_stamp = observation.get_time_stamp()
+        position = observation.get_position()
+        velocity = observation.get_velocity()
+        return time_stamp, position, velocity
